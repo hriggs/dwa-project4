25ce1af52a4535281c2c5765d69808ded4389e2c
Hannah Riggs <hkriggs128@gmail.com>
2015-12-01 14:57:17 -0500

    gamesessions migration added.

---
+++ database/migrations/2015_12_01_193928_create_gamesessions_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gamesessions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('user_id')->unsigned();
+            
+            // `user_id` is a foreign key that connects to the `id` field in the `users` table
+            $table->foreign('user_id')->references('id')->on('users');
+            
+            $table->integer('puzzle_id')->unsigned();
+            
+            // `puzzle_id` is a foreign key that connects to the `id` field in the `puzzles` table
+            $table->foreign('puzzle_id')->references('id')->on('puzzles');
+            
+            $table->integer('attempt_num');
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
+            $table->integer('moves');
+            $table->boolean('solved');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    	// drop foreign keys and columns
+    	Schema::table('books', function (Blueprint $table) {
+            $table->dropForeign('gamesessions_user_id_foreign');
+            $table->dropColumn('user_id');
+            $table->dropForeign('gamesessions_puzzle_id_foreign');
+            $table->dropColumn('puzzle_id');
+        });
+        
+        // drop table
+        Schema::drop('gamesessions');
+    }
+}
