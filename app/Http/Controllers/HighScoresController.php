07fef3de5f2aa04189c99954f1c274a281951d98
Hannah Riggs <hkriggs128@gmail.com>
2015-12-04 14:49:44 -0500

    High scores form added.

--- app/Http/Controllers/HighScoresController.php
+++ app/Http/Controllers/HighScoresController.php
@@ -19,33 +19,41 @@ class HighScoresController extends Controller {
     	// get default high scores: fastest time for first puzzle
     	$gamesessions = \App\Gamesession::with("puzzle")->orderBy("total_time", "ASC")->where("puzzle_id", "=", 1)->take(20)->get();
     	
-    	//dump($gamesessions);
+    	// get list of created puzzles
+		$puzzles = \App\Puzzle::where("created", "=", "1")->get();
     	
-    	// array to hold usernames
-    	$usernames = [];
-    	
-    	$users = \App\User::all(); 
-    	
+    	// store created puzzle titles in array
+    	$puzzle_titles = [];
+    	foreach($puzzles as $puzzle) {
+    		$puzzle_titles[] = $puzzle->title;
+    	}
+
     	// get list of gamesession user ids
     	$ids = $gamesessions->lists("user_id");
-    	
     	$k = 0; 
     	
+    	// users
+    	$usernames = [];
+    	$users = \App\User::all(); 
     	
     	// for every gamesession
     	for ($i = 1; $i < count($gamesessions) + 1; $i++) {
     		
-    		echo "In contorller: " . $i;
-    		
+			// find user associated with gamesession    		
     		$user = $users->where("id", $ids[$k])->first();
     		$k++;
     		
+    		// add username to list
     		$usernames[$i] = $user["username"];
     	}
     	
+    	// beginning rank must be 0 for index reasons in view
     	$ranking = 0;
     	
-        return view("scores.index")->with(["gamesessions"=>$gamesessions, "ranking"=>$ranking, "usernames"=>$usernames]);
+        return view("scores.index")->with(["gamesessions"=>$gamesessions, 
+        								   "ranking"=>$ranking, 
+        								   "usernames"=>$usernames,
+        								   "puzzle_titles"=>$puzzle_titles]);
     }
     
    /**
--- resources/views/scores/index.blade.php
+++ resources/views/scores/index.blade.php
@@ -5,9 +5,30 @@
 @stop
 
 @section('content')
-	<h2>High Scores</h2>
+	<h2>High Scores - Top 20</h2>
 	
 	@if(count($gamesessions) > 0)
+			<div class="sort">
+			    <form method="POST" action="/high-scores">
+					{!! csrf_field() !!}
+					<fieldset>
+						<label>See High Scores for:</label><br>
+							<select name="stats">
+								@foreach($puzzle_titles as $title)
+									<option value="{{ $title }}">{{ $title }}</option>
+								@endforeach
+							</select>
+					</fieldset>
+					<fieldset>
+						<label>Sort by:</label><br>
+							<select name="stats">
+							 	<option value="first">Fastest Time</option>
+			 					<option value="last">Least Number of moves</option>
+							</select>
+					</fieldset>
+					<input type="submit" name="sort" class="btn submit" value="Sort">
+				</form>
+			</div>
 		<table>
 			<tr>
 				<th>Ranking</th>
@@ -19,7 +40,6 @@
 			@foreach($gamesessions as $session)
 		    	<tr>
 		    		<td>{{ ++$ranking }}</td>
-		    		{{ $ranking }}
 		    		<td>{{ $usernames[$ranking] }}</td>
 		    		<td>{{ $session->puzzle->title }}</td>
 		    		<td>{{ $session->total_time }}</td>
@@ -27,7 +47,6 @@
 		        </tr>
 		    @endforeach
 	    </table>
-	
 	@else
 		<div class="page-link">
 			<p>There are no high scores yet! <a href="/register">Join</a> to get on the high score board!</p>
