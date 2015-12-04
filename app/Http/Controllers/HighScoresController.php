6da95f2de66a67b9030ac2ecc1be74cf6777cee7
Hannah Riggs <hkriggs128@gmail.com>
2015-12-04 15:57:40 -0500

    High scores page functioning.

--- app/Http/Controllers/HighScoresController.php
+++ app/Http/Controllers/HighScoresController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class HighScoresController extends Controller {
 	
@@ -19,15 +20,82 @@ class HighScoresController extends Controller {
     	// get default high scores: fastest time for first puzzle
     	$gamesessions = \App\Gamesession::with("puzzle")->orderBy("total_time", "ASC")->where("puzzle_id", "=", 1)->take(20)->get();
     	
+    	// store table headers
+    	$headers = ["Time", "Moves"]; 
+    	
     	// get list of created puzzles
 		$puzzles = \App\Puzzle::where("created", "=", "1")->get();
     	
-    	// store created puzzle titles in array
+    	// beginning rank must be 0 for index reasons in view
+    	$ranking = 0;
+    	
+        return view("scores.index")->with(["gamesessions"=>$gamesessions, 
+        								   "ranking"=>$ranking, 
+        								   "usernames"=>$this->returnUsernames($gamesessions),
+        								   "puzzle_titles"=>$this->returnTitles($puzzles),
+        								   "headers"=>$headers]);
+    }
+    
+   /**
+    * Responds to requests to POST /high-scores
+    */
+    public function postIndex(Request $request) {
+    	
+    	// store form data
+    	$puzzle_title = $request->puzzle;
+    	$sort_criteria = $request->criteria; 
+    	
+    	// get list of created puzzles
+		$puzzles = \App\Puzzle::where("created", "=", "1")->get();
+		
+		// get selected puzzle and its id
+		$puzzle = $puzzles->where("title", $puzzle_title)->first();
+		$puzzle_id = $puzzle["id"];
+		
+    	// get high scores based on form data
+    	$gamesessions = \App\Gamesession::with("puzzle")->orderBy($sort_criteria, "ASC")->where("puzzle_id", "=", $puzzle_id)->take(20)->get();
+    	
+    	$headers = [];
+    	
+    	// set up table headers based on form data
+    	if ($sort_criteria == "total_time") { 
+    		$headers = ["Time", "Moves"];
+    	} else {
+    		$headers = ["Moves", "Time"];
+    	}
+    	
+    	// beginning rank must be 0 for index reasons in view
+    	$ranking = 0;
+    	
+    	//return "Profile controller -- by POST!";
+        return view("scores.index")->with(["gamesessions"=>$gamesessions, 
+        								   "ranking"=>$ranking, 
+        								   "usernames"=>$this->returnUsernames($gamesessions),
+        								   "puzzle_titles"=>$this->returnTitles($puzzles),
+     									   "headers"=>$headers]);
+    }
+    
+   /**
+    * Return array of puzzle titles
+    */
+    public function returnTitles($puzzles) {
+    	
+    	
     	$puzzle_titles = [];
+    	
+    	// store puzzle titles in array
     	foreach($puzzles as $puzzle) {
     		$puzzle_titles[] = $puzzle->title;
     	}
-
+    	
+    	return $puzzle_titles;
+    }
+    
+   /**
+    * Return array of usernames associated with given gamesessions
+    */
+    public function returnUsernames($gamesessions) {
+    	
     	// get list of gamesession user ids
     	$ids = $gamesessions->lists("user_id");
     	$k = 0; 
@@ -47,21 +115,6 @@ class HighScoresController extends Controller {
     		$usernames[$i] = $user["username"];
     	}
     	
-    	// beginning rank must be 0 for index reasons in view
-    	$ranking = 0;
-    	
-        return view("scores.index")->with(["gamesessions"=>$gamesessions, 
-        								   "ranking"=>$ranking, 
-        								   "usernames"=>$usernames,
-        								   "puzzle_titles"=>$puzzle_titles]);
-    }
-    
-   /**
-    * Responds to requests to POST /high-scores
-    */
-    public function postIndex() {
-    	
-    	return "Profile controller -- by POST!";
-        //return view("index.index");
+    	return $usernames; 
     }
 }
--- resources/views/scores/index.blade.php
+++ resources/views/scores/index.blade.php
@@ -6,50 +6,55 @@
 
 @section('content')
 	<h2>High Scores - Top 20</h2>
+	<div class="sort">
+    <form method="POST" action="/high-scores">
+		{!! csrf_field() !!}
+		<fieldset>
+			<label>See High Scores for:</label><br>
+				<select name="puzzle">
+					@foreach($puzzle_titles as $title)
+						<option value="{{ $title }}">{{ $title }}</option>
+					@endforeach
+				</select>
+		</fieldset>
+		<fieldset>
+			<label>Sort by:</label><br>
+				<select name="criteria">
+				 	<option value="total_time">Fastest Time</option>
+ 					<option value="moves">Least Number of moves</option>
+				</select>
+		</fieldset>
+		<input type="submit" name="sort" class="btn submit" value="Sort">
+	</form>
+</div>
 	
 	@if(count($gamesessions) > 0)
-			<div class="sort">
-			    <form method="POST" action="/high-scores">
-					{!! csrf_field() !!}
-					<fieldset>
-						<label>See High Scores for:</label><br>
-							<select name="stats">
-								@foreach($puzzle_titles as $title)
-									<option value="{{ $title }}">{{ $title }}</option>
-								@endforeach
-							</select>
-					</fieldset>
-					<fieldset>
-						<label>Sort by:</label><br>
-							<select name="stats">
-							 	<option value="first">Fastest Time</option>
-			 					<option value="last">Least Number of moves</option>
-							</select>
-					</fieldset>
-					<input type="submit" name="sort" class="btn submit" value="Sort">
-				</form>
-			</div>
 		<table>
 			<tr>
 				<th>Ranking</th>
 			    <th>User</th>
 	    		<th>Puzzle</th>		
-	    		<th>Thing Counted</th>
-	    		<th>Other thing</th>
+	    		<th>{{ $headers[0] }}</th>
+	    		<th>{{ $headers[1] }}</th>
 			</tr>
 			@foreach($gamesessions as $session)
 		    	<tr>
 		    		<td>{{ ++$ranking }}</td>
 		    		<td>{{ $usernames[$ranking] }}</td>
 		    		<td>{{ $session->puzzle->title }}</td>
-		    		<td>{{ $session->total_time }}</td>
-		    		<td>{{ $session->moves }}</td>
+			    		@if ( $headers[0] == "Time")
+	    					<td>{{ $session->total_time }}</td>
+		    				<td>{{ $session->moves }}</td>
+						@else
+		    				<td>{{ $session->moves }}</td>
+		    				<td>{{ $session->total_time }}</td>
+						@endif
 		        </tr>
 		    @endforeach
 	    </table>
 	@else
 		<div class="page-link">
-			<p>There are no high scores yet! <a href="/register">Join</a> to get on the high score board!</p>
+			<p>There are no high scores for this puzzle yet! <a href="/register">Join</a> to get on the high score board!</p>
 		</div>
 	@endif
 @stop
