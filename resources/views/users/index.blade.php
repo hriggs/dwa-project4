209286b63ae6549d7afdde4f8fa8919f171fba37
Hannah Riggs <hkriggs128@gmail.com>
2015-12-14 23:23:53 -0500

    Wording fixed on users page.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -12,7 +12,7 @@
 		<p><span class="header">Country:</span><br>{{ $user->country }}</p>
 		<p><span class="header">Bio:</span><br>{{ $user->bio }}</p>
 	@else
-		<p>A valid username was not at end of URL!</p>
+		<p>A valid username was not at the end of the URL!</p>
 		<div class="page-link">
 			<p>Please visit the <a href="/high-scores">high scores page</a> to find some users.</p>
 		</div>
